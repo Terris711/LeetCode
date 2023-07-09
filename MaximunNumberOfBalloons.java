f5cc1056006ebb8fee3cc767553e01ef9c9972a3
Terris711 <terristerris200501@gmail.com>
2023-07-09 21:56:53 +0800

    Day 1 with Tai

--- MaximunNumberOfBalloons.java
+++ MaximunNumberOfBalloons.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class MaximunNumberOfBalloons {
+class MaximunNumberOfBalloons {
+    public int maxNumberOfBalloons(String text) {
+        int balloon[] = new int[5];
+
+        if(text.length() < 6){
+            return 0;
+        }
+
+        for(char c:text.toCharArray()){
+            switch(c){
+                case 'b':
+                    balloon[0]++;
+                    break;
+                case 'a':
+                    balloon[1]++;
+                    break;
+                case 'l':
+                    balloon[2]++;
+                    break;
+                case 'o':
+                    balloon[3]++;
+                    break;
+                case 'n':
+                    balloon[4]++;
+                    break;
+            }
+        }
+
+        balloon[2] /= 2;
+        balloon[3] /= 2;
+
+        if(balloon[2] <= 0 || balloon[3] <= 0){
+            return 0;
+        }
+
+        int min = balloon[0];
+
+        for (int i = 1; i < 5; i ++){
+            if(balloon[i] < min){
+                min = balloon[i];
+            }
+        }
+
+        if (min <= 0){
+            return 0;
+        }
+
+        return min;
+    }
 }
+
+
+
+
+
--- WordPattern.java
+++ WordPattern.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class WordPattern {
+import java.util.*;
+
+public class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        Map hp = new HashMap<Character, String>();
+        Map hs = new HashMap<String, Character>();
+
+        String[] words = s.split(" ");
+
+        if (words.length != pattern.length()){
+            return false;
+        }
+
+        for (int i=0; i < pattern.length(); i++){
+            char c = pattern.charAt(i);
+            if (hp.containsKey(c)){
+                if (!hp.get(c).equals(words[i])){
+                    return false;
+                }
+            }
+            hp.put(c, words[i]);
+
+            if (hs.containsKey(words[i])){
+                if (!hs.get(words[i]).equals(c)){
+                    return false;
+                }
+            }
+            hs.put(words[i], c);
+        }
+
+        return true;
+    }
 }
