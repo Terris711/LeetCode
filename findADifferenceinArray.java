1d55fe547f9a8c73af01d4d2db525cfebbe6a0a8
Terris711 <terristerris200501@gmail.com>
2023-07-11 22:27:09 +0800

    Day 3 with Tai <3

---
+++ findADifferenceinArray.java
@@ -0,0 +1,30 @@
+public class findADifferenceinArray {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> check1 = new HashSet<>();
+        Set<Integer> check2 = new HashSet<>();
+        for (int item : nums1){
+            check1.add(item);
+        }
+
+        for (int item : nums2){
+            check2.add(item);
+        }
+
+        List<List<Integer>> answer = new ArrayList<>();
+        answer.add(new ArrayList<>());
+        answer.add(new ArrayList<>());
+
+        for (int item : check1){
+            if (!check2.contains(item)){
+                answer.get(0).add(item);
+            }
+        }
+
+        for (int item : check2){
+            if (!check1.contains(item)){
+                answer.get(1).add(item);
+            }
+        }
+        return answer;
+    }
+}
