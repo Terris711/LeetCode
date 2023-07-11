8d558be4860b8c4c372e08922ba08558a7047093
Terris711 <terristerris200501@gmail.com>
2023-07-11 23:05:58 +0800

    Day 3 with Tai <3

---
+++ isIsomorphic.java
@@ -0,0 +1,26 @@
+public class isIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(map1.containsKey(s.charAt(i))){
+                if(map1.get(s.charAt(i)) != t.charAt(i)){
+                    return false;
+                }
+                continue;
+            }
+
+            if(map2.containsKey(t.charAt(i))){
+                if(map2.get(t.charAt(i)) != s.charAt(i)){
+                    return false;
+                }
+                continue;
+            }
+
+            map1.put(s.charAt(i), t.charAt(i));
+            map2.put(t.charAt(i), s.charAt(i));
+        }
+        return true;
+    }
+}
