16c002c3e01d12878e2f7bb0d8cdf39e437c7536
Terris711 <terristerris200501@gmail.com>
2023-07-12 23:27:21 +0800

    Day 4 with Tai <3

---
+++ sortColors.java
@@ -0,0 +1,23 @@
+public class sortColors {
+    public void sortColors(int[] nums) {
+        int[] arr = new int[3];
+        for (int num : nums){
+            arr[num]++;
+        }
+        int num = 0;
+        while (arr[0] > 0){
+            nums[num++] = 0;
+            arr[0]--;
+        }
+
+        while (arr[1] > 0){
+            nums[num++] = 1;
+            arr[1]--;
+        }
+
+        while(arr[2] > 0){
+            nums[num++] = 2;
+            arr[2]--;
+        }
+    }
+}
