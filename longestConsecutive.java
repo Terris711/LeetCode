840c63e3d98d04f7929ead394f49cbaccc70f436
Terris711 <terristerris200501@gmail.com>
2023-07-13 00:12:41 +0800

    Day 4 with Tai <3

---
+++ longestConsecutive.java
@@ -0,0 +1,23 @@
+public class longestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums){
+            set.add(num);
+        }
+
+        int count = 0;
+
+        for (int num : nums) {
+            if (!set.contains(num - 1)){
+                int length = 0;
+                while (set.contains(num + length)){
+                    length += 1;
+                }
+
+                count = Math.max(count, length);
+            }
+        }
+        return count;
+    }
+}
