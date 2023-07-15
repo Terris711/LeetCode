ae09866003360b236048610b0ee8d1941b24e099
Terris711 <terristerris200501@gmail.com>
2023-07-15 21:20:19 +0800

    Day 6 alone

---
+++ Array/TwoSum.java
@@ -0,0 +1,23 @@
+package Array;
+import java.util.*;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> answer = new HashMap<>();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++){
+            answer.put(nums[i], i);
+        }
+
+        for (int i = 0; i < n; i++){
+            int left = target - nums[i];
+            if (answer.containsKey(left) && answer.get(left) != i){
+                return new int[]{i, answer.get(left)};
+            }
+        }
+        return new int[]{};
+    }
+}
+
