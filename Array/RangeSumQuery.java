e4e9df8250e4052786380ccf0caeb1ce938d3d36
Terris711 <terristerris200501@gmail.com>
2023-07-15 21:42:22 +0800

    Day 7 alone

--- Array/RangeSumQuery.java
+++ Array/RangeSumQuery.java
@@ -1,6 +1,6 @@
 package Array;
 
-public class RangeSumQuery  {
+public class RangeSumQuery {
 
     //Using O(n) time complexity
     class NumArray {
@@ -13,10 +13,29 @@ public class RangeSumQuery  {
 
         public int sumRange(int left, int right) {
             int sum = 0;
-            for (int i = left; i <= right; i++){
+            for (int i = left; i <= right; i++) {
                 sum += arr[i];
             }
             return sum;
         }
     }
+
+    //Using prefixSUm
+
+
+    private int[] prefixSum;
+
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+
+        for (int i = 1; i <= nums.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+
+
 }
