7c46cf623a1e13769e14b68fc6c2bd54f55f27f0
Terris711 <terristerris200501@gmail.com>
2023-07-18 01:03:28 +0800

    Day 9 with Tai <33

--- LinkedList/getIntersectionNode.java
+++ LinkedList/getIntersectionNode.java
@@ -18,4 +18,24 @@ public class getIntersectionNode {
         }
         return null;
     }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode listA = headA;
+        ListNode listB = headB;
+
+        while( listA != listB){
+            if (listA == null){
+                listA = headB;
+            } else {
+                listA = listA.next;
+            }
+
+            if (listB == null){
+                listB = headA;
+            } else {
+                listB = listB.next;
+            }
+        }
+        return listA;
+    }
 }
