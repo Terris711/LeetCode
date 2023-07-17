b00127e4b9dcb65aff17276edf016b2d11eb063d
Terris711 <terristerris200501@gmail.com>
2023-07-18 00:26:26 +0800

    Day 9 with Tai <33

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -1,2 +1,16 @@
-package LinkedList;public class ReverseLinkedList {
+package LinkedList;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode first = null;
+        ListNode current = head;
+
+        while (current != null){
+            ListNode temp = current.next;
+            current.next = first;
+            first = current;
+            current = temp;
+        }
+        return first;
+    }
 }
