e41fafdfccb6f775246b9de0b3b2cb4b5d421c1a
Terris711 <terristerris200501@gmail.com>
2023-07-21 22:05:33 +0800

    Day 13 with Tai <33 Hard

--- BitManipulation/ReverseBits.java
+++ BitManipulation/ReverseBits.java
@@ -1,2 +1,15 @@
-package BitManipulation;public class ReverseBits {
+package BitManipulation;
+
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int answer = 0;
+
+        for (int i = 0; i < 32; i++){
+            int lastDigit = n & 1;
+            int shift = lastDigit << (31 - i);
+            answer = answer | shift;
+            n = n >> 1;
+        }
+        return answer;
+    }
 }
