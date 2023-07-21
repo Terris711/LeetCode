2fa067f4c13a9146bf2034e269a41c3091dc8234
Terris711 <terristerris200501@gmail.com>
2023-07-21 23:28:17 +0800

    Day 13 with Tai <33 SumDiagonals

--- Math/DiagonalSum.java
+++ Math/DiagonalSum.java
@@ -1,2 +1,15 @@
-package Math;public class DiagonalSum {
+package Math;
+
+public class DiagonalSum{
+    public int diagonalSum(int[][] mat) {
+        int count = 0;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat.length; col++){
+                if (row == col || row + col == mat.length - 1){
+                    count += mat[row][col];
+                }
+            }
+        }
+        return count;
+    }
 }
