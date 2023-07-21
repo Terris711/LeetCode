b3779e4426c64419130ca971126b35ca976ce6d4
Terris711 <terristerris200501@gmail.com>
2023-07-21 22:58:48 +0800

    Day 13 with Tai <33 Math

---
+++ Math/CountOdds.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class CountOdds {
+    public int countOdds(int low, int high) {
+        int count = 0;
+        if (low % 2 != 0 || high % 2 != 0){
+            count++;
+        }
+        return (high - low)/2 + count;
+    }
+}
