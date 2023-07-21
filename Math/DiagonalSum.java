d9f30fc5a08316a688fad50cb98aa41085a354ad
Terris711 <terristerris200501@gmail.com>
2023-07-21 23:38:50 +0800

    Day 13 with Tai <33 SumDiagonals improved

--- Math/DiagonalSum.java
+++ Math/DiagonalSum.java
@@ -1,6 +1,8 @@
 package Math;
 
 public class DiagonalSum{
+
+    //O(n^2)
     public int diagonalSum(int[][] mat) {
         int count = 0;
         for (int row = 0; row < mat.length; row++){
@@ -11,5 +13,18 @@ public class DiagonalSum{
             }
         }
         return count;
+    }
+
+    // O(?)
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for (int i = 0; i < n; i++){
+            sum = sum + mat[i][i];
+            if (n - i - 1 != i){
+                sum += mat[i][n-i-1];
+            }
+        }
+        return sum;
     }
 }
