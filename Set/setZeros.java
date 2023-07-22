b48a385579e98130cb1a8b7eb251ef215a7e959f
Terris711 <terristerris200501@gmail.com>
2023-07-22 22:53:15 +0800

    Day 15 with Tai <33 SetZeros

---
+++ Set/setZeros.java
@@ -0,0 +1,31 @@
+package Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class setZeros {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int r = 0; r < row; r++){
+            for (int c = 0; c < col; c++){
+                if (matrix[r][c] == 0){
+                    rows.add(r);
+                    cols.add(c);
+                }
+            }
+        }
+
+        for (int r = 0; r < row; r++){
+            for (int c = 0; c < col; c++){
+                if (rows.contains(r) || cols.contains(c)){
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+}
