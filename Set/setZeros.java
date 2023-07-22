d606c185b7b738494c8e7c9c26f9e74f118d0c2e
Terris711 <terristerris200501@gmail.com>
2023-07-22 23:23:07 +0800

    Day 15 with Tai <33 SetZeros

--- Set/setZeros.java
+++ Set/setZeros.java
@@ -1,5 +1,6 @@
 package Set;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,4 +32,43 @@ public class setZeros {
 
     //Time Complexity: O(mn)
     //Space Complexity: O(mn)
+
+    public void setZeroesOptimal(int[][] matrix) {
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int check = 0;
+
+        //Check the 1st row has 0 or not
+        while (check < col && matrix[0][check] != 0){
+            check ++;
+        }
+
+
+        // Loop through the maxtrix, ste the otehr row and col as 0
+        for (int i = 1; i < row; i++){
+            for (int j = 0; j < col; j++){
+                if (matrix[i][j] == 0){
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < row; i++){
+            for (int j = col - 1; j >= 0; j--){
+                if (matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set 1st entire array as 0)
+        if (check < col){
+            Arrays.fill(matrix[0], 0);
+        }
+
+    }
+
+    //Time Complexity: O(mn)
+    //Space Complexity: O(1)
 }
