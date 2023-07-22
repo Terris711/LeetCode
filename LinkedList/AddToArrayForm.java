841b97b109925c32c7e0ce7f0d96d0b4c84729a1
Terris711 <terristerris200501@gmail.com>
2023-07-22 21:43:31 +0800

    Day 15 with Tai <33 addToArrayForm

---
+++ LinkedList/AddToArrayForm.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddToArrayForm {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> answer = new LinkedList<>();
+
+        for (int i = num.length - 1; i >= 0; i--){
+            answer.add(0, (num[i] + k) % 10 );
+            k = (num[i] + k) / 10;
+        }
+
+        while (k > 0){
+            answer.add(0, k % 10);
+            k = k / 10;
+        }
+
+        return answer;
+    }
+}
