e347cb065284066d833af73a4dd71690caa742f0
Terris711 <terristerris200501@gmail.com>
2023-07-23 23:11:00 +0800

    Day 16 with Tai <33 Dynamic Programming

--- DynamicProgramming/MinimumCostClimbingStairs.java
+++ DynamicProgramming/MinimumCostClimbingStairs.java
@@ -13,4 +13,11 @@ public class MinimumCostClimbingStairs {
         }
         return Math.min(dp[cost.length - 1], dp[cost.length -2]);
     }
+
+    public int minCostClimbingStairsOptimal(int[] cost) {
+        for (int i = 2; i < cost.length; i++){
+            cost[i] = cost[i] + Math.min(cost[i-1],cost[i-2]);
+        }
+        return Math.min(cost[cost.length - 1], cost[cost.length -2]);
+    }
 }
