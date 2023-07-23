937ced330f00ebb5dea7682d74a81758db8a64b7
Terris711 <terristerris200501@gmail.com>
2023-07-23 23:42:35 +0800

    Day 16 with Tai <33 Dynamic Programming DivisorGame Optimal

--- DynamicProgramming/DivisorGame.java
+++ DynamicProgramming/DivisorGame.java
@@ -4,5 +4,14 @@ public class DivisorGame {
     public boolean divisorGame(int n) {
         return n % 2 == 0;
     }
-    //O(1)
+    //O(1) - Constant time
+
+    public boolean divisorGame(int n) {
+        if (n == 1){
+            return false;
+        } else {
+            return !divisorGame(n - 1);
+        }
+    }
+    //Using DP O(n)
 }
