d2b64eb21fe8e62ab722873d08206cd1c01e5747
Terris711 <terristerris200501@gmail.com>
2023-07-24 22:48:38 +0800

    Day 17 Dynamic Programming Goodnight Tai beo

---
+++ DynamicProgramming/DeleteAndEarn.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+public class DeleteAndEarn {
+    public int deleteAndEarn(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int[] memos = new int[10002];
+
+        // Sum up all nums
+        for (int num : nums){
+            memos[num] += num;
+        }
+
+        int last = 0;
+        int secondLast = 0;
+
+        for (int item : memos){
+            int temp = last;
+            last = Math.max(secondLast + item, last);
+            secondLast = temp;
+        }
+        return last;
+    }
+}
