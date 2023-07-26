44094c7b75ce2923fcbf07796317a6eff57687fb
Terris711 <terristerris200501@gmail.com>
2023-07-26 23:37:23 +0800

    Day 19 with Tai Beo <3 ReverseVowels

---
+++ Math/ReverseVowels.java
@@ -0,0 +1,29 @@
+package Math;
+
+public class ReverseVowels {
+    public String reverseVowels(String s) {
+        char[] text = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        String vowels = "ueoaiUEOAI";
+
+        while (left < right){
+            while (left < right && vowels.indexOf(text[left]) == -1){
+                left++;
+            }
+
+            while (left < right && vowels.indexOf(text[right]) == -1){
+                right--;
+            }
+
+            char temp = text[left];
+            text[left] = text[right];
+            text[right] = temp;
+
+            left++;
+            right--;
+        }
+        String answer = new String(text);
+        return answer;
+    }
+}
