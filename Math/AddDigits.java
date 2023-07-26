1bdc2cb13bd22bb938de94e7afc855f3d7eb3858
Terris711 <terristerris200501@gmail.com>
2023-07-26 23:19:28 +0800

    Day 19 with Tai Beo <3 Adđigits

---
+++ Math/AddDigits.java
@@ -0,0 +1,16 @@
+package Math;
+
+public class AddDigits {
+    public int addDigits(int num) {
+        int sum = 0;
+        while (num != 0){
+            int temp = num % 10;
+            sum += temp;
+            num = num / 10;
+        }
+        if (sum >= 10){
+            return addDigits(sum);
+        }
+        return sum;
+    }
+}
