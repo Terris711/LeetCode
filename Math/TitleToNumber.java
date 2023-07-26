286dfd65f14f9a1de1a60af5826a812d5a194582
Terris711 <terristerris200501@gmail.com>
2023-07-26 22:40:56 +0800

    Day 19 with Tai Beo <3 Math

---
+++ Math/TitleToNumber.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class TitleToNumber {
+    public int titleToNumber(String columnTitle) {
+        int answer = 0;
+        for (int i = 0; i < columnTitle.length(); i++){
+            answer = answer * 26 + (columnTitle.charAt(i) - 'A' + 1);
+        }
+        return answer;
+    }
+}
