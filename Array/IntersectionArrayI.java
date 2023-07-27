b4b09f88252b3eefea8615a4f3878c1f7c9e28ca
Terris711 <terristerris200501@gmail.com>
2023-07-27 22:17:18 +0800

    Anna & Tide day 18

---
+++ Array/IntersectionArrayI.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionArrayI {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        Set<Integer> tmp = new HashSet<>();
+
+        for (int i : nums1){
+            set1.add(i);
+        }
+
+        for (int j : nums2){
+            set2.add(j);
+        }
+
+        for (Integer i : set1){
+            if (set2.contains(i)){
+                tmp.add(i);
+            }
+        }
+
+        int[] answer = new int[tmp.size()];
+        int index = 0;
+        for (Integer i : tmp){
+            answer[index++] = i;
+        }
+        return answer;
+    }
+}
--- Math/AddDigits.java
+++ Math/AddDigits.java
@@ -3,16 +3,15 @@ package Math;
 public class AddDigits {
     public int addDigits(int num) {
         int sum = 0;
-        while (num != 0){
+        while (num != 0) {
             int temp = num % 10;
             sum += temp;
             num = num / 10;
         }
-        if (sum >= 10){
+        if (sum >= 10) {
             return addDigits(sum);
         }
         return sum;
     }
-
     //O(n^2)
 }
