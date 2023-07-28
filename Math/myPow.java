eb06ff76e25a671fe3900b40566c183e801343cd
Terris711 <terristerris200501@gmail.com>
2023-07-28 22:48:01 +0800

    Anna & Tide day 19 Geometric

---
+++ Math/myPow.java
@@ -0,0 +1,21 @@
+package Math;
+
+public class myPow {
+    public double myPow(double x, int n) {
+        //base case
+        if (n == 0) return 1;
+
+        long temp = n;
+        if (temp < 0){
+            temp = -temp;
+            x = 1/x;
+        }
+
+        if(temp % 2 == 0) {
+            return myPow(x * x, (int)(temp/2)); // x^(temp/2)
+        }
+        else {
+            return x*myPow(x, (int)(temp - 1)); // x^(temp -1)*x
+        }
+    }
+}
