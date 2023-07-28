4ef9b779812624d91ddad50adb72a4b4c8ceac94
Terris711 <terristerris200501@gmail.com>
2023-07-28 22:05:51 +0800

    Anna & Tide day 19 Geometric

---
+++ Math/IsPowerOfTwo.java
@@ -0,0 +1,10 @@
+package Math;
+
+public class IsPowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        if (n == 0) return false;
+        else if (n%2 != 0) return false;
+        else return isPowerOfTwo(n/2);
+    }
+}
