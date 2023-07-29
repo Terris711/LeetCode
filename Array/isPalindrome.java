b2889e510f4788836537207981b17f69f803ea14
Terris711 <terristerris200501@gmail.com>
2023-07-30 01:38:12 +0800

    Anna & Tide Day 21

---
+++ Array/isPalindrome.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class isPalindrome {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int n = s.length();
+
+        for (int i = 0; i < n/2; i++){
+            if (s.charAt(i) != s.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
