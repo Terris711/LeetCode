0bc55e418bf08e243a4edb33760d67fbfdac0fb0
Terris711 <terristerris200501@gmail.com>
2023-07-30 22:49:50 +0800

    Anna and Tide <3 Day 21

---
+++ Array/FindMaxAverage.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class FindMaxAverage {
+    public double findMaxAverage(int[] nums, int k) {
+        int average = 0;
+        int maxSum = 0;
+
+        for(int  i = 0;  i < k; i++){
+            maxSum += nums[i];
+        }
+
+        int sum = maxSum;
+
+        for (int i = k; i < nums.length; i++){
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(sum, maxSum);
+        }
+
+        return (double)maxSum/k;
+    }
+}
