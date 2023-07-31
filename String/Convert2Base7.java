22ed342955b4aef28229c8ee8336fc7caaa43662
Terris711 <terristerris200501@gmail.com>
2023-07-31 23:23:17 +0800

    Anna and Tide <3 Day 22

--- String/Convert2Base7.java
+++ String/Convert2Base7.java
@@ -7,3 +7,19 @@ public class Convert2Base7 {
 }
 
 //O(1)
+
+
+    public String convertToBase7(int num) {
+        int count = 0;
+        int result = 0;
+
+        while (num != 0){
+            int digit = num % 7;
+            result = result + digit * (int) Math.pow(10, count);
+            count ++;
+            num /= 7;
+        }
+        return Integer.toString(result);
+    }
+
+    //O(n)
