ace9f2eb5756a256d8670aa23e942668c82d303b
Terris711 <terristerris200501@gmail.com>
2023-08-01 22:45:32 +0800

    Anna vs Tide Day 23

---
+++ Array/TeemoAttacking.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class TeemoAttacking {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int answer = duration;
+
+        for (int i = 1; i < timeSeries.length; i++){
+            if (timeSeries[i] <= timeSeries[i -1] + duration){
+                answer += timeSeries[i] - timeSeries[i - 1];
+            } else {
+                answer += duration;
+            }
+        }
+        return answer;
+    }
+}
