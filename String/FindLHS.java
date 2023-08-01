4e44e7b19fd886b373d970fab99b9ffd026cea95
Terris711 <terristerris200501@gmail.com>
2023-08-01 23:16:34 +0800

    Anna vs Tide Day 23 <3

---
+++ String/FindLHS.java
@@ -0,0 +1,30 @@
+package String;
+
+import java.util.Arrays;
+
+public class FindLHS {
+    public int findLHS(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 0;
+        int result = 0;
+
+        while (right < nums.length){
+            int diff = nums[right] - nums[left];
+            if (diff == 1){
+                result = Math.max(result, right - left + 1);
+            }
+
+            if(diff <= 1){
+                right++;
+            } else {
+                left++;
+            }
+        }
+        return result;
+    }
+}
