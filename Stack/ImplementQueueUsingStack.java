eb805056cb61c6a6ce5b16035df4f78f64cf0445
Terris711 <terristerris200501@gmail.com>
2023-08-01 21:32:14 +0800

    Anna vs Tide Day 23

---
+++ Stack/ImplementQueueUsingStack.java
@@ -0,0 +1,40 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStack {
+    Stack<Integer> stackin = new Stack();
+    Stack<Integer> stackout = new Stack();
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        if(stackin.isEmpty()){
+            stackin.push(x);
+            return;
+        }
+
+        while (!stackin.isEmpty()){
+            stackout.push(stackin.pop());
+        }
+
+        stackin.push(x);
+        while(!stackout.empty()){
+            stackin.push(stackout.pop());
+        }
+    }
+
+    public int pop() {
+        return stackin.pop();
+    }
+
+    public int peek() {
+        return stackin.peek();
+    }
+
+    public boolean empty() {
+        return stackin.isEmpty() && stackout.isEmpty();
+    }
+}
