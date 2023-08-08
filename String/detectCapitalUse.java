13775fa76a3992a4082e5a4b6fb45c2bc5f194f6
Terris711 <terristerris200501@gmail.com>
2023-08-08 20:57:32 +0800

    With Tide day 28 <3 Capital Use

---
+++ String/detectCapitalUse.java
@@ -0,0 +1,9 @@
+package String;
+
+public class detectCapitalUse {
+
+    //Use Buil-in function
+    public boolean detectCapitalUse(String word) {
+        return word.toUpperCase().equals(word) || word.substring(1).toLowerCase().equals(word.substring(1));
+    }
+}
