bb04008069aa4ee1b6a256705f8079dd7ce303a5
Terris711 <terristerris200501@gmail.com>
2023-08-08 21:57:10 +0800

    With Tide day 28 <3 maxCount

---
+++ Array/maxCount.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class maxCount {
+    public int maxCount(int m, int n, int[][] ops) {
+        int minRow = m;
+        int minCol = n;
+
+        for (int[] op : ops){
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+        return minRow * minCol;
+    }
+}
