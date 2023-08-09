e5433b2d058528d96449e000d6870efe41144e09
Terris711 <terristerris200501@gmail.com>
2023-08-09 22:54:48 +0800

    Day 30 with Tai find Error nums optimize

--- Array/findErrorNums.java
+++ Array/findErrorNums.java
@@ -23,4 +23,25 @@ public class findErrorNums {
         return answer;
     }
     //O(n)
+
+    public int[] findErrorNumsOptimize(int[] nums) {
+        int[] answer = new int[2];
+        int n = nums.length;
+        boolean[] visited = new boolean[nums.length + 1];
+
+        HashSet<Integer> set = new HashSet<>();
+        int sum = 0;
+        for (int num : nums){
+            if (visited[num]){
+                answer[0] = num;
+            } else {
+                visited[num] = true;
+                sum += num;
+            }
+        }
+
+        answer[1] = (n*(n+1)/2) - sum;
+
+        return answer;
+    }
 }
