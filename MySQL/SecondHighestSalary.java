6b037d17cf3c11ca5fbc4510d005045dc09fd42a
Terris711 <terristerris200501@gmail.com>
2023-08-10 23:40:45 +0800

    I cant wait to hug u

---
+++ MySQL/SecondHighestSalary.java
@@ -0,0 +1,9 @@
+package MySQL;
+
+public class SecondHighestSalary {
+    # Write your MySQL query statement below
+    SELECT (
+            SELECT DISTINCT salary FROM Employee
+            ORDER BY salary DESC LIMIT 1 OFFSET 1
+    ) as SecondHighestSalary;
+}
