a0a67f44c713aa43483b622f781f2229407442ee
Terris711 <terristerris200501@gmail.com>
2023-08-11 22:50:01 +0800

    Day 31 I love you

---
+++ MySQL/EarningMoreThanManager.java
@@ -0,0 +1,8 @@
+package MySQL;
+
+public class EarningMoreThanManager {
+    # Write your MySQL query statement below
+    SELECT name as Employee
+    FROM Employee e
+    WHERE salary > (SELECT salary FROM Employee WHERE id = e.managerId)
+}
