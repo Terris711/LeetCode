9d11921fde59733daabc4e5132e84b88dc3603fb
Terris711 <terristerris200501@gmail.com>
2023-08-12 20:04:02 +0800

    Day 32 I <3 u

---
+++ MySQL/DuplicateEmail.java
@@ -0,0 +1,8 @@
+package MySQL;
+
+public class DuplicateEmail {
+    String result = "# Write your MySQL query statement below\n" +
+            "SELECT email FROM Person\n" +
+            "GROUP BY email\n" +
+            "HAVING COUNT(email) > 1;";
+}
