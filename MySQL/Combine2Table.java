70708794c87983851e85203d220a7e58d02fb797
Terris711 <terristerris200501@gmail.com>
2023-08-12 21:14:26 +0800

    Day 32 I <3 u

--- MySQL/Combine2Table.java
+++ MySQL/Combine2Table.java
@@ -4,3 +4,4 @@ public class Combine2Table {
     String result = "SELECT firstname, lastName, city, state\n" +
             "FROM Person p LEFT JOIN Address a ON p.personId = a.personId;";
 }
+//878
