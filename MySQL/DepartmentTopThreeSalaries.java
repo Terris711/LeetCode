b8e31637156ab817eb7aff62ffad639142644c0a
Terris711 <terristerris200501@gmail.com>
2023-08-12 21:49:32 +0800

    Day 32 I hate u

---
+++ MySQL/DepartmentTopThreeSalaries.java
@@ -0,0 +1,13 @@
+package MySQL;
+
+public class DepartmentTopThreeSalaries {
+    String result = "SELECT d.name as Department, e.name as Employee, e.salary\n" +
+            "FROM Employee as e\n" +
+            "INNER JOIN Department as d\n" +
+            "ON d.id = e.departmentId\n" +
+            "WHERE (\n" +
+            "    SELECT COUNT(DISTINCT(salary)) \n" +
+            "    FROM Employee\n" +
+            "    WHERE salary > e.salary AND DepartmentId = e.DepartmentId\n" +
+            ") < 3;";
+}
