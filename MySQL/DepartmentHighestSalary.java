203b2195b7995559a0c545dad6d72c590413bb39
Terris711 <terristerris200501@gmail.com>
2023-08-13 20:20:20 +0800

    Day 33

---
+++ MySQL/DepartmentHighestSalary.java
@@ -0,0 +1,9 @@
+package MySQL;
+
+public class DepartmentHighestSalary {
+    String result = "SELECT d.name AS Department, e.name as Employee, e.salary\n" +
+            "FROM Employee e, Department D\n" +
+            "WHERE e.departmentId = d.id\n" +
+            "AND (departmentId, salary) IN (SELECT departmentId, max(salary) as Salary \n" +
+            "                                FROM Employee GROUP BY departmentId);";
+}
