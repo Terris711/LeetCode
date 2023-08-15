9ec761fbd5ca3df9c16a567d682957b5b574449d
Terris711 <terristerris200501@gmail.com>
2023-08-15 23:28:04 +0800

    Day 35

---
+++ MySQL/ConsecutiveNumber.java
@@ -0,0 +1,9 @@
+package MySQL;
+
+public class ConsecutiveNumber {
+    String result = "SELECT DISTINCT log1.num as ConsecutiveNums\n" +
+            "FROM Logs as log1\n" +
+            "JOIN Logs as log2 ON log1.id = log2.id - 1\n" +
+            "JOIN Logs as log3 ON log1.id = log3.id - 2\n" +
+            "WHERE log1.num = log2.num and log1.num = log3.num";
+}
