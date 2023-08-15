88fed2c5ea85366f0151c2f8ecc95982e7a09178
Terris711 <terristerris200501@gmail.com>
2023-08-15 13:57:43 +0800

    Day 35

--- String/LongestFrefix.java
+++ String/LongestFrefix.java
@@ -11,10 +11,10 @@ public class LongestFrefix {
 
             Arrays.sort(strs);
             String start = strs[0];
-            String end = strs[strs.length - 1];
+            String end s= strs[strs.length - 1];
 
             int index = 0;
-            while (index < start.length() && index < end.length()){
+            while (index < ==start.length() && index < end.length()){
                 if (start.charAt(index) == end.charAt(index)){
                     index++;
                 } else {
@@ -25,4 +25,22 @@ public class LongestFrefix {
         }
     }
     //O(n) with java
+}
+
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs.Length == 0) return "";
+
+        string prefix = "";
+
+        for (int i = 0; i < strs[0].Length; i++){
+            foreach(string str in strs){
+                if (i > str.Length - 1) return prefix;
+                if (strs[0][i] != str[i]) return prefix;
+            }
+            prefix += strs[0][i];
+        }
+        return prefix;
+    }
 }
