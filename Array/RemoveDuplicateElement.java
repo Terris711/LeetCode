9daa324b07abb43ac9fe81e8617a38e08db20449
Terris711 <terristerris200501@gmail.com>
2023-08-16 14:35:38 +0800

    Day 36 with him

--- Array/RemoveDuplicateElement.java
+++ Array/RemoveDuplicateElement.java
@@ -16,3 +16,16 @@ public class RemoveDuplicateElement {
 }
 
 //O(n) with Java
+
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.Length; i++){
+            if (nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
