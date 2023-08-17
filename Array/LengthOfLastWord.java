43a5ffb2337f08390df9cfcc3e8ce6d7a5856f30
Terris711 <terristerris200501@gmail.com>
2023-08-17 10:05:28 +0800

    Day 37

---
+++ Array/LengthOfLastWord.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class LengthOfLastWord {
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            String string = s.trim();
+            s = s.trim();
+            int count = 0;
+            int i = s.length() - 1;
+            while (i >= 0 && string.charAt(i) != ' '){
+                count++;
+                i--;
+            }
+            return count;
+        }
+    }
+}
