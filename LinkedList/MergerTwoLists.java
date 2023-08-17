1a333ce7338288fde9c30846d4d33fafa1a5bbdb
Terris711 <terristerris200501@gmail.com>
2023-08-17 08:10:22 +0800

    Day 37

--- LinkedList/MergerTwoLists.java
+++ LinkedList/MergerTwoLists.java
@@ -33,3 +33,36 @@ public class MergerTwoLists {
 }
 
 // Using recursive with O(n) Java
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        // Base case
+        if (list1 != null && list2 != null) {
+            // Using recursive
+            if (list1.val < list2.val){
+                list1.next = MergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = MergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+
+        if (list1 == null) return list2;
+        else return list1;
+    }
+}
+
+// O(n) using C#
