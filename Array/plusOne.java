bd3c1acc1734acfd54502fd2514fb806843e48c7
Terris711 <terristerris200501@gmail.com>
2023-08-17 12:26:17 +0800

    Day 37

--- Array/plusOne.java
+++ Array/plusOne.java
@@ -17,3 +17,21 @@ public class plusOne {
     }
 }
 //O(n) with java
+
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        for (int i = digits.Length - 1; i >= 0; i--){
+            if (digits[i] < 9){
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        digits = new int[digits.Length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
+
+
