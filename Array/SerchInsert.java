8618a55aed999d58571f64c8579d622a0fa2f37b
Terris711 <terristerris200501@gmail.com>
2023-08-17 09:05:33 +0800

    Day 37

--- Array/SerchInsert.java
+++ Array/SerchInsert.java
@@ -18,3 +18,18 @@ public class SerchInsert {
 }
 
 // O(nlogn) with java
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.Length - 1;
+
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) return mid;
+            if (nums[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return start;
+    }
+}
