3c178de47a976badf25905ede36436915cc59cf4
Terris711 <terristerris200501@gmail.com>
2023-08-18 22:49:53 +0800

    Day 37

---
+++ String/SimplifyPath.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.util.Stack;
+
+public class SimplifyPath {
+    class Solution {
+        public String simplifyPath(String path) {
+            Stack<String> stack = new Stack<String>();
+
+            for (String s : path.split("/")){
+                if (s.equals("..")){
+                    if (!stack.empty()) stack.pop();
+                } else if (!s.equals(".") && !s.equals("")){
+                    stack.push(s);
+                }
+            }
+            return "/" + String.join("/", stack);
+        }
+    }
+}
