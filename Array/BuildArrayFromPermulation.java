1922518d095b732e93e3df784f1de861e04c8618
Terris711 <terristerris200501@gmail.com>
2023-08-19 23:11:33 +0800

    Day 38

---
+++ Array/BuildArrayFromPermulation.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class BuildArrayFromPermulation {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+            int[] result = new int[nums.length];
+
+            for(int i = 0; i < result.length; i++){
+                result[i] = nums[nums[i]];
+            }
+            return result;
+        }
+    }
+}
