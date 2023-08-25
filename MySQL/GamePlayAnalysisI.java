cb0f6c3150819b966ec5ad1440555c30564d467f
Terris711 <terristerris200501@gmail.com>
2023-08-25 14:13:32 +0800

    Day 43

---
+++ MySQL/GamePlayAnalysisI.java
@@ -0,0 +1,8 @@
+package MySQL;
+
+public class GamePlayAnalysisI {
+    String result = "# Write your MySQL query statement below\n" +
+            "SELECT player_id, min(event_date) as first_login\n" +
+            "FROM Activity\n" +
+            "GROUP BY player_id"
+}
