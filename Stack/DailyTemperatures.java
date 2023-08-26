f03977cc56422fc640005074b62a455f502994bc
Terris711 <terristerris200501@gmail.com>
2023-08-26 23:17:18 +0800

    Day 44 Stack

---
+++ Stack/DailyTemperatures.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> stack = new Stack<>();
+
+        int res[] = new int[n];
+
+        for (int i = 0; i < n; i++){
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                res[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push();
+        }
+        return res;
+    }
+}
