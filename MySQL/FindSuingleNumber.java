6b47b5722556166d1b388335b8dfcc3b830afcc5
Terris711 <terristerris200501@gmail.com>
2023-08-27 21:37:53 +0800

    Day 45 Tai is sleeping bruh bruh

---
+++ MySQL/FindSuingleNumber.java
@@ -0,0 +1,7 @@
+package MySQL;
+
+public class FindSuingleNumber {
+    String result = "# Write your MySQL query statement below\n" +
+            "SELECT max(num) as num FROM Mynumbers\n" +
+            "WHERE num not in (SELECT num FROM Mynumbers GROUP BY num HAVING COUNT(num) > 1)";
+}
