3624f159171e754c9c90121c9cfab325854818f1
Terris711 <terristerris200501@gmail.com>
2023-08-28 06:38:57 +0800

    Day 46 on the train ...

---
+++ MySQL/ProductSalesAnalysisI.java
@@ -0,0 +1,7 @@
+package MySQL;
+
+public class ProductSalesAnalysisI {
+    String res = "SELECT p.product_name, s.year, s.price FROM Sales as s\n" +
+            "LEFT JOIN Product as p\n" +
+            "ON p.product_id = s.product_id;";
+}
