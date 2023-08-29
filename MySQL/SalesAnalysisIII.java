64f45e31bd342791d1c7bb16551a3f02fa3ed0e8
Terris711 <terristerris200501@gmail.com>
2023-08-30 07:58:12 +0800

    Day 48

---
+++ MySQL/SalesAnalysisIII.java
@@ -0,0 +1,9 @@
+package MySQL;
+
+public class SalesAnalysisIII {
+    String res ="SELECT p.product_id, p.product_name FROM Product as p\n" +
+            "LEFT JOIN Sales as s\n" +
+            "ON p.product_id = s.product_id\n" +
+            "GROUP BY p.product_id\n" +
+            "HAVING MIN(s.sale_date) >= \"2019-01-01\" AND MAX(s.sale_date) <= \"2019-03-31\";";
+}
