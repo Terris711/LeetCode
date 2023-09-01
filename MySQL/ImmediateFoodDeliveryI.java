7ff7f5f8d88394487a0f3ff31356a2a2cb3d0056
Terris711 <terristerris200501@gmail.com>
2023-09-01 13:56:20 +0800

    Day 49 ImmediateFoodDeliveryI 1099

--- MySQL/ImmediateFoodDeliveryI.java
+++ MySQL/ImmediateFoodDeliveryI.java
@@ -4,3 +4,4 @@ public class ImmediateFoodDeliveryI {
     String res = "SELECT ROUND(100*AVG(order_date = customer_pref_delivery_date),2) as immediate_percentage \n" +
             "FROM Delivery;"
 }
+//1099
