74e5be311adb0e9f270a19b7f2a53248b686c816
Terris711 <terristerris200501@gmail.com>
2023-09-01 14:09:36 +0800

    Day 49 ReformatDepartmentTable

---
+++ MySQL/ReformatDepartmentTable.java
@@ -0,0 +1,20 @@
+package MySQL;
+
+public class ReformatDepartmentTable {
+    String res ="# Write your MySQL query statement below\n" +
+            "SELECT id, \n" +
+            "sum(if(month = 'Jan', revenue, null)) as Jan_Revenue,\n" +
+            "sum(if(month = 'Feb', revenue, null)) as Feb_Revenue,\n" +
+            "sum(if(month = 'Mar', revenue, null))as Mar_Revenue,\n" +
+            "sum(if(month = 'Apr', revenue, null)) as Apr_Revenue,\n" +
+            "sum(if(month = 'May', revenue, null)) as May_Revenue,\n" +
+            "sum(if(month = 'Jun', revenue, null)) as Jun_Revenue,\n" +
+            "sum(if(month = 'Jul', revenue, null)) as Jul_Revenue,\n" +
+            "sum(if(month = 'Aug', revenue, null)) as Aug_Revenue,\n" +
+            "sum(if(month = 'Sep', revenue, null)) as Sep_Revenue,\n" +
+            "sum(if(month = 'Oct', revenue, null)) as Oct_Revenue,\n" +
+            "sum(if(month = 'Nov', revenue, null)) as Nov_Revenue,\n" +
+            "sum(if(month = 'Dec', revenue, null)) as Dec_Revenue\n" +
+            "FROM Department\n" +
+            "GROUP BY id;\n";
+}
