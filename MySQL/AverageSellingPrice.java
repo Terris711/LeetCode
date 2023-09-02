f33ce378cc9da5a3efb38d85279f3be065cfddcc
Terris711 <terristerris200501@gmail.com>
2023-09-02 22:28:09 +0800

    Day 50 sql

---
+++ MySQL/AverageSellingPrice.java
@@ -0,0 +1,10 @@
+package MySQL;
+
+public class AverageSellingPrice {
+    String res = "SELECT p.product_id, ROUND(SUM(price*units)/SUM(units), 2) as average_price\n" +
+            "FROM Prices as p\n" +
+            "INNER JOIN UnitsSold as u\n" +
+            "ON p.product_id = u.product_id\n" +
+            "AND u.purchase_date BETWEEN start_date AND end_date\n" +
+            "GROUP BY product_id;";
+}
