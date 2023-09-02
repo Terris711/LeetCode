5cd19cc4ba8567a61d6020da09197cab305f1171
Terris711 <terristerris200501@gmail.com>
2023-09-02 21:53:38 +0800

    Day 50

---
+++ Array/bulbSwitch.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class bulbSwitch {
+    class Solution {
+        public int bulbSwitch(int n) {
+            int count = 0;
+            for (int i = 1; i*i <= n; i++){
+                count ++;
+            }
+            return count;
+        }
+    }
+}
