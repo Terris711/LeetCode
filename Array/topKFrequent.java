b299d94431a0e93939e0ef4b15f514075ffcba78
Thi Van Anh Duong <anht@cbh.com.au>
2024-03-07 21:30:47 +0800

    Find K top frequent

--- Array/topKFrequent.java
+++ Array/topKFrequent.java
@@ -33,3 +33,36 @@ public class topKFrequent {
 }
 
 // O(NlogN)
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<>();
+            }
+            buckets[frequency].add(key);
+        }
+        int[] res = new int[k];
+
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (k == 0) break;
+            List<Integer> li = buckets[i];
+            if (li == null) continue;
+            for (Integer ans : li) {
+                res[k - 1] = ans;
+                k--;
+            }
+
+        }
+        return res;
+    }
+}
+
+//O(N)
